d829bf92187544c3738fb112b133eeef51b5df52
Mateus Samartini <mateusamartini@gmail.com>
2022-05-24 19:18:57 -0300

    aaaaaaaaa

---
+++ beck/src/test/java/FanForum/FunForumApplicationTests.java
@@ -0,0 +1,13 @@
+package FanForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
